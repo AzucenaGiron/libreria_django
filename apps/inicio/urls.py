7ed311b28eb390d0368e82ab49a00ddeebebb298
EduardoBorraz <sep_eduardo@outlook.com>
2019-12-17 04:39:58 -0600

    borraz
    17 dic 2019

--- apps/inicio/urls.py
+++ apps/inicio/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from .api.routers import router
-from .views import vista_login
+from .views import vista_login,vista_inicio,vista_libros
 from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns = [
-    #path('',inicio_vista,name='inicio'),
+    path('',vista_inicio,name='inicio'),
     path('api/',include(router.urls)),
     #path('login/',vista_login,name="vistalogin"),
-    path('login/',LoginView.as_view(template_name="libros/libros.html"),name='login'),
+    path('login/',LoginView.as_view(template_name="inicio/index.html"),name='login'),
     path('cerrar/',LogoutView.as_view(template_name="inicio/login.html"),name="logout"),
+    path('libros/',vista_libros,name="libros")
+   
 ]
--- apps/inicio/views.py
+++ apps/inicio/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def vista_login(request):
     return render(request,"inicio/login.html")
+def vista_inicio(request):
+    return render(request,"inicio/index.html")
+def vista_libros(request):
+    return render(request,"libros/libros.html")    
